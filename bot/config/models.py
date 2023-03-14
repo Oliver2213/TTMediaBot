bc1c44e88f4b7b7276d7a95893eb44e5405b0b65
Blake Oliver <oliver22213@me.com>
2023-03-13 23:55:22 -0400

    Add field to CronEntryModel for the parsed instance; add comments for those attributes

--- bot/config/models.py
+++ bot/config/models.py
@@ -95,7 +95,11 @@ class ShorteningModel(BaseModel):
 
 
 class CronEntryModel(BaseModel):
+    # Text cron pattern
     pattern: str = ""
+    # CronTab parsed instance
+    entry: optional[CronTab] = None
+    # What to run when this cron entry matches
     command: str = ""
 
     def valid_pattern(self) -> bool:
